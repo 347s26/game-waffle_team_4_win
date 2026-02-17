de0ed7a6e4679bf6e025b807f08f92706dd79ca2
Aiden <aidensmith385@gmail.com>
2026-02-17 11:56:27 -0500

    Merge branch 'main' of https://github.com/347s26/game-waffle_team_4_win

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-
+    path("test1/", views.test_one, name="test_one"),
+    path("test2/", views.test_two, name="test_two"),
+    path("test3/", views.test_three, name="test_three"),
+    path("test4/", views.test_four, name="test_four"),
 ]
